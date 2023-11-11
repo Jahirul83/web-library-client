649b84bd3f5fd9115085f98739d162ce11a31d17
Jahirul83 <jahirulislamcse83@gmail.com>
2023-11-11 14:47:25 +0600

    router setup and home page, login page and register page created

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,29 @@
+
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../layout/Root";
+import Home from "../components/Home/Home";
+import Login from "../components/Login/Login";
+import Register from "../components/Register/Register";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<Root></Root>,
+      children: [
+        {
+            path:"/",
+            element:<Home></Home> 
+        },
+        {
+            path:"/login",
+            element: <Login></Login>
+        },
+        {
+            path:"/register",
+            element: <Register></Register>
+        }
+      ],
+    },
+  ]);
+
+  export default router;
