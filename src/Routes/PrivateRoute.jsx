26205e65afde3bbb94ffe3172fac82d5ab1368c4
Jahirul83 <jahirulislamcse83.com>
2023-11-14 19:48:46 +0600

    Google login implemented

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -5,7 +5,7 @@ import { AuthContext } from '../providers/AuthProvider';
 const PrivateRoute = ({ children }) => {
     const { loading } = useContext(AuthContext);
     if (loading) {
-        return <progress className="progress w-56" value="100" max="100"></progress>
+        return <progress className="progress w-56" value="100" max="100"></progress>;
     }
     return children;
 };
