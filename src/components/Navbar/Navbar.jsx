1062dd847ea2fcb6d9e776159727452e3e649f0b
Jahirul83 <jahirulislamcse83@gmail.com>
2023-11-14 21:27:59 +0600

    banner design implemented

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
                         {navItems}
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <a className="btn btn-ghost normal-case text-xl">BOOKS <span className="text-indigo-600">POINT</span></a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
