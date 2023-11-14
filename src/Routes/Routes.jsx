54574e950c0d6dac1a0c5455118b630d84027814
Jahirul83 <jahirulislamcse83@gmail.com>
2023-11-15 03:27:48 +0600

    category card added to the home page and book will be sort as the category_name from the database

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,8 @@ import Root from "../layout/Root";
 import Home from "../components/Home/Home";
 import Login from "../components/Login/Login";
 import Register from "../components/Register/Register";
+import Categories from "../components/Categories/Categories";
+import Books from "../components/Books/Books";
 
 const router = createBrowserRouter([
     {
@@ -12,7 +14,9 @@ const router = createBrowserRouter([
       children: [
         {
             path:"/",
-            element:<Home></Home> 
+            element:<Home></Home>,
+            loader: ()=> fetch('categories.json')
+             
         },
         {
             path:"/login",
@@ -21,7 +25,16 @@ const router = createBrowserRouter([
         {
             path:"/register",
             element: <Register></Register>
-        }
+        },
+        {
+            path:"/categories",
+            element: <Categories></Categories>,
+            loader: ()=> fetch('categories.json') 
+        },
+        {
+            path:"/books/:category_name",
+            element: <Books></Books>
+        },
       ],
     },
   ]);
---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,20 @@
+import { useLoaderData } from "react-router-dom";
+import Category from "./Category";
+
+
+const Categories = () => {
+    const categories = useLoaderData();
+    return (
+        <div>
+            {/* <h2>category:{categories.length}</h2> */}
+            {
+                categories.map(category => <Category
+                    key={category.id}
+                    category={category}
+                ></Category>)
+            }
+        </div>
+    );
+};
+
+export default Categories;
