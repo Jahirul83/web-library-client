8ce1eaf519bcef0ba7f27368ff9306506b419c0d
Jahirul83 <jahirulislamcse83.com>
2023-11-15 19:05:16 +0600

    Books shown by category

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,10 +1,34 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router-dom";
+import BooksCard from "./BooksCard";
 
 const Books = () => {
+    const books = useLoaderData();
+    const [remaining, setRemaining] = useState([]);
+    // const [category, setCategory] = useState([]);
+    // const [currentCategory, setCurrentCategory] = useState([]);
     const { category_name } = useParams();
+
+    useEffect(() => {
+
+
+        let rem =  books.filter(filterData => category_name.toLowerCase() === filterData.category.toLowerCase());
+        setRemaining(rem);
+        // console.log(rem);
+
+    }, [category_name,books])
+
+
     return (
         <div>
-            <h2>this is books:{category_name.toLowerCase()}</h2>
+            <h2>this is books:{remaining.length}</h2>
+            {
+                remaining?.map(bookData => 
+                <BooksCard 
+                    key={bookData.id}
+                    bookData={bookData}
+                    ></BooksCard>)
+            }
         </div>
     );
 };
